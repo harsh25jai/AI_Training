2ec0052125f4c193b02159719dc012db9e1201aa
Harsh Jaiswal <harshjaiswal@MacBook-Pro-19.local>
2026-03-27 12:55:27 +0530

    deleted node_modules from poc

--- poc/ai_customer_Assistant/app/node_modules/retell-sdk/models/errors/sdkerror.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class SDKError extends Error {
-    statusCode: number;
-    body: string;
-    rawResponse: Response;
-    constructor(message: string, response: Response, body?: string);
-}
-//# sourceMappingURL=sdkerror.d.ts.map
